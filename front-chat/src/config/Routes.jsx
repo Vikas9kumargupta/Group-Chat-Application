39a887f88ac05cb2efa877f0d4aa2316ad01ea97
Vikas Kumar Gupta <vikasgup074@gmail.com>
2025-03-02 16:21:37 +0530

    Update Changes.

--- front-chat/src/config/Routes.jsx
+++ front-chat/src/config/Routes.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { Route, Routes } from "react-router";
+import { Routes, Route } from "react-router";
 import App from "../App";
-
+import ChatPage from "../components/ChatPage";
 const AppRoutes = () => {
-    return (
-        <Routes>
-          <Route path="/" element={<App/>} />
-          <Route path="/about" element={<h1>This is about Page !! </h1>} />
-          <Route path="/contact" element={<h1>This is Contact Page !!</h1>} />
-          <Route path="*" element={<h1>Error 404 ! Page Not Found</h1>}/>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/chat" element={<ChatPage />} />
+      <Route path="/about" element={<h1>This is about page</h1>} />
+      <Route path="*" element={<h1>404 Page Not Found</h1>} />
+    </Routes>
+  );
 };
 
 export default AppRoutes;

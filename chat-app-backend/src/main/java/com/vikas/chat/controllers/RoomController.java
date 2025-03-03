f7c3d9188461ba1c7388c9c3e7dc413f6137a829
Vikas Kumar Gupta <vikasgup074@gmail.com>
2025-03-04 02:25:30 +0530

    Final Commit

--- chat-app-backend/src/main/java/com/vikas/chat/controllers/RoomController.java
+++ chat-app-backend/src/main/java/com/vikas/chat/controllers/RoomController.java
@@ -5,7 +5,6 @@ import com.vikas.chat.config.AppConstants;
 import com.vikas.chat.entities.Message;
 import com.vikas.chat.entities.Room;
 import com.vikas.chat.repositories.RoomRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
